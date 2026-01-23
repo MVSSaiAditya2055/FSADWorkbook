3ec9808953a9d219173b2b0f8d2e1a3e05520d6b
MVSSaiAditya2055 <2400032055@kluniversity.in>
2026-01-23 07:42:15 +0530

    done

---
+++ Workbook5/src/main/java/com/klu/model/Student.java
@@ -0,0 +1,36 @@
+package com.klu.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	private int studentId;
+	private String name;
+	private String gender;
+	@Autowired
+	private Certification certificate;
+	
+	@Autowired
+	public Student() {
+		this.studentId=2055;
+		this.name="Aditya";
+		this.gender="Male";
+	}
+	
+	public Student(int studentId, String name, String gender) {
+        this.studentId = studentId;
+        this.name = name;
+        this.gender = gender;
+    }
+	
+	public void display() {
+		System.out.println("following are the certification details:");
+		System.out.println("StudentId:"+studentId);
+		System.out.println("Name:"+name);
+		System.out.println("Gender:"+gender);
+		System.out.println("CertificateId:"+certificate.getId());
+		System.out.println("CertificateName:"+certificate.getName());
+		System.out.println("DateOfCompletion:"+certificate.getDate());
+	}
+}
